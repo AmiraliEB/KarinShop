82b5e0ac19ba1e81ecc456fa8fec0fc3d922e23b
Amir <amirali21800@gmail.com>
2025-10-25 13:55:57 +0330

    feat:implement form and view for cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 
-class CartView(TemplateView):
-    template_name = "cart/cart.html"
+from .forms import CartAddPrproductForm
+from .cart import Cart
+from django.shortcuts import get_object_or_404
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request,'cart/cart.html')
+    def post(self,request, *args, **kwargs):
+        form = CartAddPrproductForm(request.POST)
+        cart = Cart(request)
+        pk = self.kwargs['pk']
+        product = get_object_or_404()
+
+        if form.is_valid():
+            cart.add()
