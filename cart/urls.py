fea76ebb8bd29c85d8bf972f62df463c59bce19e
Amir <amirali21800@gmail.com>
2025-10-26 20:03:30 +0330

    feat:add RemoveCartItemView and fix cart detail page

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CartView
+from .views import CartView, RemoveCartItemView
 
 urlpatterns = [
-    path('cart/',CartView.as_view(),name='cart_detail')
+    path('cart/',CartView.as_view(),name='cart_detail'),
+    path('cart/remove/<int:pk>',RemoveCartItemView.as_view(),name='cart_remove_item'),
+    path('cart/clear',RemoveCartItemView.as_view(),name='cart_clear_item'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render,redirect
+from django.views.generic import View,TemplateView
 
 from .forms import CartAddPrproductForm
 from .cart import Cart
+from products.models import Product
 from django.shortcuts import get_object_or_404
 
-class CartView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request,'cart/cart.html')
-    def post(self,request, *args, **kwargs):
-        form = CartAddPrproductForm(request.POST)
-        cart = Cart(request)
-        pk = self.kwargs['pk']
-        product = get_object_or_404()
+class CartView(TemplateView):
+    template_name = 'cart/cart.html'
 
-        if form.is_valid():
-            cart.add()
+class RemoveCartItemView(View):
+    def get(self,request,*args, **kwargs):
+        cart = Cart(request)
+        pk = self.kwargs.get('pk')
+        if pk is None:
+           return redirect('cart_detail')
+        product = Product.objects.get(pk=pk)
+        cart.remove(product)
+        return redirect('cart_detail')
+
+    def post(self,request,*args, **kwargs):
+        # post method is for clear all the items
+        cart = Cart(request)
+        cart.clear()
+        return redirect('cart_detail')
