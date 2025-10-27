7385dd7f0cb2f121073dee71ca8e39c4da2c366d
Amir <amirali21800@gmail.com>
2025-10-27 11:47:41 +0330

    fix:delete all color model and fields

--- products/admin.py
+++ products/admin.py
@@ -9,9 +9,9 @@ class ProductImageInline(admin.TabularInline):
 class ProductInline(admin.TabularInline):
     model = models.Product
     extra = 0  
-    fields = ('price', 'discount_price', 'stock', 'is_available', 'color', 'attribute_values')
+    fields = ('price', 'discount_price', 'stock', 'is_available', 'attribute_values')
     readonly_fields = ('is_available',)
-    filter_horizontal = ('color', 'attribute_values')
+    filter_horizontal = ('attribute_values',)
 
 class AttributeRuleInline(admin.TabularInline):
     model = models.AttributeRule
@@ -46,7 +46,7 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('parent_product__name', 'id')
     readonly_fields = ('full_name', 'datetime_created', 'datetime_modified', 'is_available')
     autocomplete_fields = ('parent_product',)
-    filter_horizontal = ('color', 'attribute_values',)
+    filter_horizontal = ('attribute_values',)
     list_select_related = ('parent_product',)
 
     def full_name(self, obj):
@@ -71,11 +71,6 @@ class AttributeValueAdmin(admin.ModelAdmin):
     list_filter = ('attribute',)
     ordering = ('attribute__name', 'value')
 
-@admin.register(models.Color)
-class ColorAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
 @admin.register(models.Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -87,7 +82,7 @@ class AttributeCategory(admin.ModelAdmin):
 
 @admin.register(models.Comments)
 class CommentsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'parent_product', 'rating', 'is_recommend', 'datetime_created')
+    list_display = ('title', 'user', 'parent_product', 'rating', 'is_recommend', 'datetime_created','is_approved')
     list_filter = ('is_recommend', 'rating', 'datetime_created')
     search_fields = ('title', 'content', 'parent_product__name')
     readonly_fields = ('datetime_created', 'datetime_modified')
