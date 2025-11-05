88a4e48317a79a486a4cdbc27af3a932b05c981b
Amir <amirali21800@gmail.com>
2025-11-05 21:40:47 +0330

    fix:migration confilict and migrate new model

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from accounts.models import Address
+from django.core.validators import RegexValidator
 class CartAddPrproductForm(forms.Form):
     quantity = forms.IntegerField(
         min_value=1,
@@ -16,12 +17,21 @@ class CartAddPrproductForm(forms.Form):
     )
 
 class CartAddAddressFrom(forms.Form):
+    phone_number_validator = RegexValidator(
+        regex= r'^09[0-9]{9}$',
+        message= "شماره موبایل باید ۱۱ رقمی باشد و با 09 شروع شود.",
+    )
+    postal_code_validator = RegexValidator(
+    regex=r'^[0-9]{10}$',
+    message="کد پستی باید ۱۰ رقمی و فقط شامل اعداد باشد."
+    )
+
     first_name = forms.CharField(max_length=50, required=False)
     last_name = forms.CharField(max_length=50, required=False)
-    
+
     province = forms.CharField(max_length=50, required=True)
     city = forms.CharField(max_length=50, required=True)
-    postal_code = forms.IntegerField(required=False)
-    full_address = forms.CharField(verbose_name=_("full address"), max_length=255)
-    phone_number = forms.IntegerField(required=False)
+    postal_code = forms.IntegerField(required=False, validators=[postal_code_validator])
+    full_address = forms.CharField(max_length=255)
+    phone_number = forms.IntegerField(required=True,validators=[phone_number_validator])
 
