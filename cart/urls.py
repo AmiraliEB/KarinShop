66ba9c716dc64ba40a3693297face3f20a87f4f0
Amir <amirali21800@gmail.com>
2025-11-06 16:28:30 +0330

    feat:add view for chackout and payment page

--- cart/urls.py
+++ cart/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart/',views.CartView.as_view(),name='cart_detail'),
-    path('cart/remove/<int:pk>',views.RemoveCartItemView.as_view(),name='cart_remove_item'),
-    path('cart/clear',views.RemoveCartItemView.as_view(),name='cart_clear_item'),
-    path('cart/checkout',views.CheckoutView.as_view(),name='checkout'),
+    path('',views.CartView.as_view(),name='cart_detail'),
+    path('/remove/<int:pk>',views.RemoveCartItemView.as_view(),name='cart_remove_item'),
+    path('/clear',views.RemoveCartItemView.as_view(),name='cart_clear_item'),
+    path('/checkout',views.CheckoutView.as_view(),name='checkout'),
+    path('/payment',views.PaymentView.as_view(),name='payment'),
 ]
