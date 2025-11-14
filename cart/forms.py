9a5cd73a792caece862f7d843aefaebac0662679
Amir <amirali21800@gmail.com>
2025-11-14 15:13:47 +0330

    feat:add forms to template

--- cart/forms.py
+++ cart/forms.py
@@ -16,8 +16,14 @@ class CartAddPrproductForm(forms.Form):
         )
     )
 
+
 class CartAddAddressFrom(forms.Form):
-    
+    SHIPPING_CHOICES = [
+        ('post', 'پست پیشتاز: 89,000 تومان'),
+        ('tipax', 'تیپاکس: 120,000 تومان'),
+    ]
+
+
     phone_number_validator = RegexValidator(
         regex= r'^09[0-9]{9}$',
         message= "شماره موبایل باید ۱۱ رقمی باشد و با 09 شروع شود.",
@@ -29,13 +35,23 @@ class CartAddAddressFrom(forms.Form):
 
     tailwind_classes = "block w-full py-1.5 px-3 text-base outline dark:outline-none outline-1 -outline-offset-1 placeholder:text-gray-400 transition-all text-gray-800 dark:text-gray-100 dark:bg-gray-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded-md outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100 dark:focus:ring-blue-400 h-11"
 
-    first_name = forms.CharField(max_length=50, required=False,label=_("First name"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'نام*'}))
-    last_name = forms.CharField(max_length=50, required=False, label=_("Last name"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'نام خانوادگی*'}))
+    first_name = forms.CharField(max_length=50, required=True,label=_("First name"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'نام*'}))
+    last_name = forms.CharField(max_length=50, required=True, label=_("Last name"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'نام خانوادگی*'}))
 
     province = forms.CharField(max_length=50, required=True, label=_("Province"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'استان*'}))
     city = forms.CharField(max_length=50, required=True, label=_("City"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'شهر*'}))
-    postal_code = forms.IntegerField(required=False, validators=[postal_code_validator], label=_("Postal code"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'کد پستی'}))
+    postal_code = forms.IntegerField(required=True, validators=[postal_code_validator], label=_("Postal code"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'کد پستی*'}))
     full_address = forms.CharField(max_length=255, required=True, label=_("Full address"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'آدرس*'}))
-    phone_number = forms.IntegerField(required=True,validators=[phone_number_validator], label=_("Phone number"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'تلفن*'}))
+    phone_number = forms.IntegerField(required=False,validators=[phone_number_validator], label=_("Phone number"),widget=forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'تلفن'}))
 
     description = forms.CharField(max_length=500, label=_("Description"), required=False, widget= forms.TextInput(attrs={"class":tailwind_classes,"placeholder":'توضیحات'})) 
+
+
+    shipping_method = forms.ChoiceField(
+        choices=SHIPPING_CHOICES,
+        widget=forms.RadioSelect(attrs={
+            'class': 'hidden peer'
+        }),
+        initial='post',
+        required=True
+    )
--- dockerfile
+++ dockerfile
@@ -3,10 +3,16 @@ FROM python:3.11-slim-bookworm
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN apt-get update && apt-get install -y gettext
+RUN apt-get update && \
+    apt-get install -y gettext locales && \
+    sed -i -e 's/^# \(fa_IR UTF-8\)/\1/' /etc/locale.gen && \
+    LC_ALL=C dpkg-reconfigure --frontend=noninteractive locales && \
+    rm -rf /var/lib/apt/lists/*
+ENV LANG fa_IR.UTF-8
+ENV LANGUAGE fa_IR:fa
+ENV LC_ALL fa_IR.UTF-8
 
 WORKDIR /app
-
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
