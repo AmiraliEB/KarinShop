09cd81535409230da91e4672336c0fd3f77723a5
Amir <amirali21800@gmail.com>
2025-11-16 14:28:44 +0330

    feat:add Cart and CartItem model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+User = get_user_model()
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=models.CASCADE)
+
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_("creation date"))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("last modified date"))
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, verbose_name=_("cart"), on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('products.Product', verbose_name=_("product"), on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name=_("quantity"), default=1)
+
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_("creation date"))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("last modified date"))
+    
+    def __str__(self):
+        return f"{self.quantity} عدد از {self.product.name} در سبد {self.cart.user.username}"
+    
+    def get_total_price(self):
+        return self.product.price * self.quantity
