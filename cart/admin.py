74bce67ea952b327bacd16c3ea3d645e58489124
Amir <amirali21800@gmail.com>
2025-11-16 16:03:21 +0330

    feat:add signal for merge session cart to db cart

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'datetime_created', 'datetime_modified')
+    inlines = [CartItemInline]
---
+++ cart/signals.py
@@ -0,0 +1,37 @@
+from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
+from .models import Cart, CartItem
+from products.models import Product
+
+@receiver(user_logged_in)
+def merge_session_cart_to_db_cart(sender, request, user, **kwargs):
+    session_cart = request.session.get('cart', {})
+    if not session_cart:
+        return
+
+    try:
+        db_cart, created = Cart.objects.get_or_create(user=user)
+
+        for product_id_str, session_item_data in session_cart.items():
+            product_id = int(product_id_str)
+            session_quantity = session_item_data.get('quantity', 1)
+            try:
+                product = Product.objects.get(id=product_id)
+                db_item, item_created = CartItem.objects.get_or_create(
+                    cart=db_cart,
+                    product=product
+                )
+                if item_created:
+                    db_item.quantity = session_quantity
+                else:
+                    db_item.quantity += session_quantity
+                
+                db_item.save()
+
+            except Product.DoesNotExist:
+                continue
+        del request.session['cart']
+        request.session.modified = True
+
+    except Exception as e:
+        print(f"Error merging cart: {e}")
