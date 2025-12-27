866827dc47c78d82b4d488c4a5190bb52f0a4dba
Amir <amirali21800@gmail.com>
2025-12-27 11:22:32 +0330

    feat: add shop template

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -6,4 +6,6 @@ app_name = 'products'
 urlpatterns = [
     path('product-<int:pk>/', views.post_redirect_view, name='post_redirect'),
     path('product-<int:pk>/<str:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+
+    path('shop/', views.ShopView.as_view(), name='shop'),
 ]
