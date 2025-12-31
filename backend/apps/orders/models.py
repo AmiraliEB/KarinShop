e2beb43caf540404925782d7d142b32590f7eb11
Amir <amirali21800@gmail.com>
2025-12-31 18:35:32 +0330

    fix: $j not found error in docker (cause: $j in secret_key env file)

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -1,6 +1,4 @@
 import nanoid
-from accounts.models import Address
-from cart.models import Cart, CartItem
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
