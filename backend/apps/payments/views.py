a2a4953afc2f68871d6a43d142382bd0f4a53bae
Amir <amirali21800@gmail.com>
2026-01-01 15:31:21 +0330

    feat: complete gateway

--- backend/apps/payments/views.py
+++ backend/apps/payments/views.py
@@ -9,32 +9,36 @@ from orders.models import Coupon, Order
 
 @require_POST
 def demo_gateway_view(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        amount = 0
-        ref_id = str(uuid.uuid4().int)[:10]
-        order_number = request.GET.get("order_number")
-        user = request.user
-        if not user.is_authenticated:
-            return render(
-                request,
-                "payments/failed-payment.html",
-                {"order_number": order_number, "amount": amount, "ref_id": ref_id},
-            )
-        order = Order.objects.filter(order_number=order_number, user=user).first()
-        if order is None:
-            return render(
-                request,
-                "payments/failed-payment.html",
-                {"order_number": order_number, "amount": amount, "ref_id": ref_id},
-            )
-        coupon_id = request.session.get("coupon_id")
-        amount = order.get_total_price
-        if coupon_id is not None:
-            coupon = Coupon.objects.filter(id=coupon_id).first()
-            if coupon is not None:
-                amount, discount_amount = coupon.get_discount_amount(amount)
+    user = request.user
+    if not user.is_authenticated:
+        return render(request, "payments/failed.html", {"error": "کاربر وارد نشده است."})
+    address = Address.objects.filter(user=user).first()
+    if address is None:
+        return render(request, "payments/failed.html", {"error": "آدرس برای کاربر یافت نشد."})
+    order = Order.objects.create(
+        user=user,
+        province=address.province,
+        city=address.city,
+        postal_code=address.postal_code,
+        full_address=address.full_address,
+        phone_number=address.phone_number,
+    )
+    order = order.create_order(user=user, address=address)
 
-        context = {"order_number": order_number, "amount": amount, "ref_id": ref_id}
+    ref_id = str(uuid.uuid4().int)[:10]
+    amount = order.get_total_price
+    coupon_id: int | None = request.session.get("coupon_id")
+    if coupon_id is not None:
+        coupon = Coupon.objects.filter(id=coupon_id).first()
+        if coupon is not None:
+            amount, discount_amount = coupon.get_discount_amount(amount)
+
+    context = {
+        "order_id": order.order_number,
+        "amount": amount,
+        "ref_id": ref_id,
+    }
+
     return render(request, "payments/demo_gateway.html", context=context)
 
 
