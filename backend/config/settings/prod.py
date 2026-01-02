933d5b6c1fccd9b37f3172c324f24ab0236438fe
Amir <amirali21800@gmail.com>
2026-01-02 17:24:27 +0330

    config: enable HTTPS and update Nginx/Django settings

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,3 +1,9 @@
 from .base import *  # noqa
 
 DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
