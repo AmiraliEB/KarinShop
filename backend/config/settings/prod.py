a694ffd14b427607f5a0f9ac0495a4871c55a97c
Amir <amirali21800@gmail.com>
2026-01-04 12:50:36 +0330

    fix: combined expression error (cause: F(stock) and available check in product model save method)

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -7,3 +7,8 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://karinapp.ir",
+    "https://www.karinapp.ir",
+]
