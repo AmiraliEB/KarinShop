d791c954e4d387e198918c88b841bf68b4bf28c6
Amir <amirali21800@gmail.com>
2026-01-19 11:16:25 +0330

    fix: replace name product to product_variant in both context name and template var

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -19,6 +19,7 @@ class HomePageView(View):
         )
         context["amazing_product_variants"] = amazing_product_variants
 
+<<<<<<< HEAD
         latest_product_variants = product_variant.order_by("datetime_modified")[:6]
         context["latest_product_variants"] = latest_product_variants
         best_selling = product_variant.annotate(
@@ -27,6 +28,25 @@ class HomePageView(View):
             )
         )
         context["best_selling_product_variants"] = best_selling.order_by("paid_items_count")
+=======
+        latest_product_variants = ProductVariant.objects.filter(is_available=True).order_by("datetime_modified")[:6]
+        context["latest_product_variants"] = latest_product_variants
+        context["best_selling_product_variants"] = (
+            ProductVariant.objects.select_related("parent_product")
+            .annotate(order_item_count=Count("orderitem", filter=Q(orderitem__order__is_paid=True)))
+            .order_by("-order_item_count")[:6]
+        )
+        # hot_products = (
+        #     ProductVariant.objects.prefetch_related()
+        #     .annotate(count_cart_items=Count("cart_items"))
+        #     .order_by("count_cart_items")[:15]
+        # )
+
+        # def hot_product():
+        #     for product in hot_products:
+        #         yield product
+
+>>>>>>> cd6827d (fix: replace name product to product_variant in both context name and template var)
         context["hot_product_variants"] = None
         context["hot_product_variants_column"] = range(4)
 
