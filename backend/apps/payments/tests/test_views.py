5eb0fd498c63ce7363cd72022b05104d89f712cd
Amir <amirali21800@gmail.com>
2026-02-10 11:33:59 +0330

    test: fix all cart test and fix payment test

--- backend/apps/payments/tests/test_views.py
+++ backend/apps/payments/tests/test_views.py
@@ -55,8 +55,6 @@ class TestPaymentVerifyView:
         response = client.post(url, data)
 
         assert response.status_code == 200
-        for t in response.templates:
-            print(t.name)
         assert "payments/successful-payment.html" in [t.name for t in response.templates]
 
         order.refresh_from_db()
