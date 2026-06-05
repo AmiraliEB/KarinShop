2619d8123d0411e775d23cd5e84c6a01e5203d5d
Amir <amirali21800@gmail.com>
2026-06-05 18:01:54 +0330

    feat: finish category section and fix mobile view

--- backend/apps/products/filters.py
+++ backend/apps/products/filters.py
@@ -7,14 +7,21 @@ class ProductFilter(django_filters.FilterSet):
     available = django_filters.BooleanFilter(field_name="is_available", lookup_expr="exact")
     ordering = django_filters.OrderingFilter(
         fields=(
-            ("-paid_items_count", "best_seller"),
-            ("-recent_sales", "popular"),
+            ("paid_items_count", "best_seller"),
+            ("recent_sales", "popular"),
             (
                 "min_final_price",
                 "price",
             ),  # min_final_price is a field in ProductVariant model that returns the minimum price of the variant's products
         ),
     )
+    category = django_filters.CharFilter(field_name="parent_product__category__code", lookup_expr="exact")
+    has_discount = django_filters.BooleanFilter(method="filter_has_discount")
+
+    def filter_has_discount(self, queryset, name, value):
+        if value:
+            return queryset.filter(products__discount_value__gt=0).distinct()
+        return queryset
 
     class Meta:
         model = ProductVariant
