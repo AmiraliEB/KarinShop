d50912cd06b7c6ba39affeb149fb786fbe511f64
Amir <amirali21800@gmail.com>
2026-09-06 19:38:49 +0330

    fix: bug in filter for shop

--- backend/apps/products/filters.py
+++ backend/apps/products/filters.py
@@ -25,19 +25,4 @@ class ProductFilter(django_filters.FilterSet):
 
     class Meta:
         model = ProductVariant
-        fields = ["is_amazing", "is_available"]
-
-    # def filter_queryset(self, queryset):
-    #     data = self.request.GET.copy()
-    #     first_key = None
-    #     for key in data.keys:
-    #         if data[key]:
-    #             first_key = key
-    #             break
-    #     if first_key:
-    #         for key in list(data.keys()):
-    #             if key != first_key:
-    #                 del data[key]
-    #     self._form = self.get_form_class()(data=data)
-    #     self._form.is_valid()
-    #     return super().filter_queryset(queryset)
+        fields = []
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,7 @@ from accounts.views import (
     ResendConfirmationEmailView,
 )
 from allauth.account.views import PasswordResetView
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
