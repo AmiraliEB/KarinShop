a760aea0a9b2b923efca3eaba85de16d27ff6f82
Amir <amirali21800@gmail.com>
2026-09-12 11:15:53 +0330

    feat: compelete dashboard pages

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.HomePageView.as_view(), name="homepage"),
     path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
     path("dashboard/address/", views.DashboardAddressView.as_view(), name="dashboard-address"),
+    path("dashboard/messages/", views.DashboardMessagesView.as_view(), name="dashboard-messages"),
     path("about/", views.AboutPageView.as_view(), name="about"),
     path("contact/", views.ContactPageView.as_view(), name="contact"),
     path("questions/", views.QuestionPageView.as_view(), name="questions"),
